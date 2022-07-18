c0e13f278a3d96b7e40559b22f82eab7347127a6
Makan <m.macalou522@gmail.com>
2022-07-18 15:25:15 +0200

    debut responsive

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -5,29 +5,52 @@ import styles from './navbar.module.css';
 
 export const Navbar = () => {
 	return (
-		<div className={styles.navbar}>
+		<div className={styles.navbar}> 
 			<div className={styles.liste1}>
+				<div className={styles.logo}>
+					<Image 
+						width={30}
+						height={30}
+						src={'/assets/icons/right-arrow.png'}
+
+					/>
+					<div className={styles.bold}> Home Services </div>
+
+				</div>
 				<ul>
-					<li className={styles.logo}> Home services </li>
-					<li>Home</li>
+					<li>Home</li> 
 					<li>Categories</li>
 					<li>Help & Support</li>
 				</ul>
 
-				<ul>
-					<li>Select your adress</li>
+				<ul> 
 					<li>English</li>
 					<li>Login</li>
 					<button className={styles.btn}>Register</button>
 				</ul>
 			</div>
 			{/* <img src="public/burger-bar.png" class="menu-burger" alt="Menu Burger"/>  */}
-			<Image
-				width={50}
-				height={50}
-				src={'/assets/icons/navIcons.png'}
-				className={styles.menuBurger} 
-			/>
+
+			{/* img menu burger */} 
+			<div className={styles.menuBurger}> 
+				<Image
+					width={20}
+					height={20} 
+					src={'/assets/icons/navIcons.png'} 
+					
+				/>
+
+			</div>
+
+			{/* img localisation */} 
+			<div className={styles.map}> 
+				<Image 
+					width={25}
+					height={25}
+					src={'/assets/icons/localisation.png'} 
+				/>
+				<div className={styles.menu_title}>Select your adress</div>  
+			</div>
 		</div>
 	);
 };
